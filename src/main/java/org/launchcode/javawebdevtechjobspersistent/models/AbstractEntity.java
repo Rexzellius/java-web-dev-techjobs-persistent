c0ed195c2374a87ffbdf4f1cadb060564c0417a3
Rexzellius <katlynmmills@gmail.com>
2021-02-03 15:14:30 -0600

    dd

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -1,12 +1,18 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
-
+//@MappedSuperclass
 public abstract class AbstractEntity {
 
+//    @Id
+//    @GeneratedValue
     private int id;
 
+    @NotBlank(message = "Field required.")
+    @Size(min = 2, max = 100)
     private String name;
 
     public int getId() {
