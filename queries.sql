647668479012f8890f8107386c50ed9559137d21
Rexzellius <katlynmmills@gmail.com>
2021-02-10 19:19:18 -0600

    finished

--- queries.sql
+++ queries.sql
@@ -15,8 +15,8 @@ WHERE location = "St. Louis City";
 DROP TABLE job;
 
 ## Part 4: Test it with SQL
-SELECT name, description
+SELECT distinct name, description
 FROM skill
 INNER JOIN job_skill ON skill.id=job_skill.skill_id
-WHERE jobs_id IS NOT NULL
-ORDER BY name;
+WHERE job_skill.jobs_id IS NOT NULL
+ORDER BY skill.name;
